b0212b0f94ba12d366b2e776a5793ef0f1a01647
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2022-10-25 12:03:52 -0300

    feat: auth0 security implementation

---
+++ auth0-security.go
@@ -0,0 +1,70 @@
+package auth0security
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"time"
+
+	jwtMiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/jwks"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"github.com/gin-gonic/gin"
+)
+
+type DefaultCustomClaims struct {
+	UserRole           string `json:"user_role"`
+	RoleInOrganization string `json:"role_in_organization"`
+	Organization       string `json:"organization"`
+	OrganizationId     string `json:"organization_id"`
+	ginContext         *gin.Context
+}
+
+func (c *DefaultCustomClaims) Validate(ctx context.Context) error {
+	c.ginContext.Set("customClaims", *c)
+	return nil
+}
+
+func DefaultCustomClaimsConstructor(ginContext *gin.Context) func() validator.CustomClaims {
+	customClaims := new(DefaultCustomClaims)
+	customClaims.ginContext = ginContext
+	return func() validator.CustomClaims {
+		return customClaims
+	}
+}
+
+func JwtValidator(issuerURL *url.URL,
+	cacheTTL time.Duration,
+	audience []string,
+	customClaimsConstructor func(ginContext *gin.Context) func() validator.CustomClaims) (gin.HandlerFunc, error) {
+	provider := jwks.NewCachingProvider(issuerURL, time.Duration(5*time.Minute))
+
+	handlerFunction := func() gin.HandlerFunc {
+		return func(gctx *gin.Context) {
+			jwtValidator, err := validator.New(provider.KeyFunc,
+				validator.RS256,
+				issuerURL.String(),
+				audience,
+				validator.WithCustomClaims(customClaimsConstructor(gctx)))
+			if err != nil {
+				panic(err)
+			}
+			jwtMiddleware := jwtMiddleware.New(jwtValidator.ValidateToken)
+			var skip = true
+			var handler http.HandlerFunc = func(http.ResponseWriter, *http.Request) {
+				skip = false
+			}
+
+			jwtMiddleware.CheckJWT(handler).ServeHTTP(gctx.Writer, gctx.Request)
+			switch {
+			case skip:
+				gctx.Abort()
+			default:
+				gctx.Next()
+			}
+
+		}
+	}
+	return handlerFunction(), nil
+
+}
